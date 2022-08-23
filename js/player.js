ffff01a7d04b73377004620e3d73978d78645949
Neeta24 <neeta.sinha.24@gmail.com>
2022-08-23 13:10:31 +0600

    added select full

--- js/player.js
+++ js/player.js
@@ -1,10 +1,32 @@
 const allPlayers =[];
 
+function display(playerdetails){
+ const selectedPlayers =document.getElementById("selected-players");
+ selectedPlayers.innerHTML ='';
+
+
+    for(let i =0;i<playerdetails.length;i++){
+        // console.log(allPlayers[i]);
+        const name= allPlayers[i].playerName;
+        const tr = document.createElement("tr");
+        tr.innerHTML=
+        `<th>${i+1}</th>
+        <td>${name}</td>`;
+        selectedPlayers.appendChild(tr);
+    }
+}
+
+
+
+
+
 function players(element){
     const playerName = element.parentNode.parentNode.children[0].innerText;
     // console.log(playerName);
     const playerobj = {playerName: playerName};
     allPlayers.push(playerobj);
-     const playerlenght = allPlayers.length;
-     console.log(playerlenght);
+    //  const playerlenght = allPlayers.length;
+    //  console.log(playerlenght);
+     display(allPlayers);
+   
 }
