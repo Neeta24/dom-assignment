380b0c4d38294164bab85667ae69f65aea72db36
Neeta24 <neeta.sinha.24@gmail.com>
2022-08-23 14:52:55 +0600

    budget added

--- js/player.js
+++ js/player.js
@@ -17,9 +17,6 @@ function display(playerdetails){
 }
 
 
-
-
-
 function players(element){
     const playerName = element.parentNode.parentNode.children[0].innerText;
     // console.log(playerName);
